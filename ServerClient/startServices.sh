1fd3bcc3093241ebff9db0d41a9615bd243b92ec
dan@emagineers.co.uk <dan@emagineers.co.uk>
2015-04-17 15:10:54 +0100

    Add Mongo Client

--- ServerClient/startServices.sh
+++ ServerClient/startServices.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 node ServerClient/color-service.js &
 node ServerClient/color-service2.js &
-
 node ServerClient/twitterService.js &
 node ServerClient/mailService.js &
+node ServerClient/databaseService.js &
+
