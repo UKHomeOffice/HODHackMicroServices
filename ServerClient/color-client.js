763b040d4c2117e99aec43f64d61673ba3b40848
Brett Minnie <github@bdb-studios.co.uk>
2015-04-17 13:46:28 +0100

    Plugged in the mail service

--- ServerClient/color-client.js
+++ ServerClient/color-client.js
@@ -3,6 +3,7 @@ var seneca = require('seneca')()
 var services = {
   color1 : 10171,
   color2 : 10173,
+  mailer : 10172,
   twitter : 10174
 };
 
@@ -35,6 +36,14 @@ seneca.client(services.twitter).act(
     'cmd:tweet,tweet:some amaxing tweet - ' + new Date(),
 
     // Callback for what to do with the response
+    function doCallBack(args, res) {
+        console.log(res);
+    }
+);
+
+seneca.client(services.mailer).act(
+    'role:mail, cmd:prepare, ' +
+    'args:{to:"hod.hacker@gmail.com", subject:"Commandline Test", name:"Some Test Guy"}',
     function doCallBack(args, res) {
         console.log(res);
     }
--- ServerClient/startServices.sh
+++ ServerClient/startServices.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 node ServerClient/color-service.js &
 node ServerClient/color-service2.js &
